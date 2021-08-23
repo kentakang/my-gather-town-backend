64e1b15157412497eb24b61e66c0ea96e7217eb2
Chan Kang <me@kentakang.com>
2021-08-23 10:19:41 +0900

    feat: add hello warp

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
-fn main() {
+use warp::Filter;
 
+#[tokio::main]
+async fn main() {
+    let hello = warp::any().map(|| "Hello, warp!");
+
+    warp::serve(hello).run(([127, 0, 0, 1], 3030)).await;
 }
