15fcca09b4ca5ebd694d6cb6d3780946c6db1289
Chan Kang <me@kentakang.com>
2021-08-24 11:17:03 +0900

    feat: add websocket path

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,26 @@
+use futures::{FutureExt, StreamExt};
 use warp::Filter;
 
 #[tokio::main]
 async fn main() {
-    let hello = warp::any().map(|| "Hello, warp!");
+    let index = warp::path::end().map(|| "Hello, warp!");
+    let websocket_hello = warp::path("echo")
+        .and(warp::ws())
+        .map(|ws: warp::ws::Ws| {
+            ws.on_upgrade(|websocket| {
+                let (tx, rx) = websocket.split();
 
-    warp::serve(hello).run(([127, 0, 0, 1], 3030)).await;
+                rx.forward(tx).map(|result| {
+                    if let Err(e) = result {
+                        eprintln!("websocket error: {:?}", e);
+                    }
+                })
+            })
+        });
+    let routes = warp::get()
+        .and(
+            index.or(websocket_hello)
+        );
+    
+    warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
 }
